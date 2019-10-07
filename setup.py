7026450c2ef0d1301f54d655b58200c906cf1c8e
reinvantveer <rein.van.t.veer@geodan.nl>
2019-10-07 14:02:48 +0200

    use pipenv for dependencies, import setup reqs from pipfile

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 import setuptools
+import toml
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('Pipfile', 'rt') as p:
+    dependencies_toml = p.read()
+    dependencies = toml.loads(dependencies_toml)
+    dependencies = dependencies['packages']
+    dependency_packages = dependencies.keys()
+
+
 setuptools.setup(
     name="deep-geometry",
-    version="1.0.0",
+    version="2.0.0",
     author="Rein van 't Veer",
     author_email="rein@geodan.nl",
     description="A python library for preprocessing geospatial vector geometries for use in deep learning",
@@ -13,8 +21,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/SPINlab/deep-geometry",
     packages=setuptools.find_packages(),
-    install_requires=['numpy', 'shapely'],
-    python_requires='>=3',
+    install_requires=dependency_packages,
+    python_requires='>=3.6',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
