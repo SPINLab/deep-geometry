e141ef0137300e74c5c0f3bdd2418b2deff6a02f
reinvantveer <rein.van.t.veer@geodan.nl>
2019-10-07 14:16:27 +0200

    class-based Scaler object modeled after scikit-learn normalizers + tests, added support for different padding types, allow overriding mean centering or std deviation scaling

--- deep_geometry/geom_scaler.py
+++ deep_geometry/geom_scaler.py
@@ -1,59 +1,98 @@
-import numpy as np
+from typing import Optional
+
+import numpy
 from deep_geometry.vectorizer import FULL_STOP_INDEX
 
-
-def fit(vectors):
-    means = localized_mean(vectors)
-    min_maxs = []
-
-    for index, data_point in enumerate(vectors):
-        full_stop_point = data_point[:, FULL_STOP_INDEX].tolist()
-
-        try:
-            full_stop_point_index = full_stop_point.index(1)
-        except Exception as e:  # if a dummy point is encountered
-            min_maxs.append([0, 0])
-            continue
-
-        min_maxs.append([
-            np.min(data_point[..., :full_stop_point_index, :2] - means[index]),
-            np.max(data_point[..., :full_stop_point_index, :2] - means[index])
-        ])
-
-    return np.std(min_maxs)
+PADDING_TYPES = ['replication', 'zero']
 
 
-def transform(vectors, scale=None):
-    localized = np.copy(vectors)
-    means = localized_mean(vectors)
+def localized_mean(geometry_vector: numpy.ndarray) -> numpy.ndarray:
+    """
+    calculates a centroid for the
+    :param geometry_vector:
+    :return:
+    """
+    assert type(geometry_vector) == numpy.ndarray, 'Please provide a numpy ndarray'
+    assert numpy.ndim(geometry_vector) == 2, 'Please provide a 2d numpy ndarray'
 
-    for index, data_point in enumerate(localized):
-        full_stop_point = data_point[:, FULL_STOP_INDEX].tolist()
+    full_stop_point_index = get_full_stop_index(geometry_vector)
 
-        try:
-            full_stop_point_index = full_stop_point.index(1)
-        except Exception as e:  # if a dummy point is encountered
-            continue
-
-        data_point[..., :full_stop_point_index + 1, :2] -= means[index]
-        data_point[..., :full_stop_point_index + 1, :2] /= scale
-
-    return localized
+    # Take the mean of all points before the full stop point for localized origin
+    geom_mean = numpy.mean(geometry_vector[0:full_stop_point_index, 0:2], axis=0)
+    return numpy.array(geom_mean)
 
 
-def localized_mean(vectors):
-    geom_means = []
-    for data_point in vectors:
-        full_stop_point = data_point[:, FULL_STOP_INDEX].tolist()
+def get_full_stop_index(geometry_vector: numpy.ndarray) -> numpy.ndarray:
+    """
+    Retrieves the index in the geometry for the first full stop occurrence
+    :param geometry_vector: a geometry vector (use vectorize_wkt for this)
+    :return: the first full stop occurrence in the geometry
+    """
+    full_stop_slice = geometry_vector[..., FULL_STOP_INDEX]
+    full_stop_point_index = numpy.where(full_stop_slice == 1.)[0][0]
 
-        try:
-            full_stop_point_index = full_stop_point.index(1)
-        except Exception as e:  # if a dummy point is encountered
-            geom_means.append([[[0, 0]]])
-            continue
+    if full_stop_point_index == 0:
+        full_stop_point_index = -1
 
-        # Take the mean of all non-null points for localized origin
-        geom_mean = np.mean(data_point[0:full_stop_point_index, 0:2], axis=0, keepdims=True)
-        geom_means.append(geom_mean)
+    return full_stop_point_index
 
-    return np.array(geom_means)
+
+class GeomScaler:
+    def __init__(self) -> None:
+        self.scale_factor: Optional[float] = None
+        self.geom_means = None
+        self.min_maxs = None
+
+    def fit(self, geometry_vectors: numpy.ndarray) -> None:
+        assert type(geometry_vectors) == numpy.ndarray, 'Please provide a numpy ndarray'
+        assert numpy.ndim(geometry_vectors) == 3, 'Please provide a 3d numpy ndarray ' \
+                                                  'with axes 0:batch, 1:geometries, 2:points'
+
+        means = [localized_mean(v) for v in geometry_vectors]
+        min_maxs = []
+
+        for index, geometry in enumerate(geometry_vectors):
+            full_stop_point_index = get_full_stop_index(geometry)
+
+            x_and_y_coords = geometry[:full_stop_point_index, :2]
+            min_maxs.append([
+                numpy.min(x_and_y_coords - means[index]),
+                numpy.max(x_and_y_coords - means[index])
+            ])
+
+        self.scale_factor = numpy.std(min_maxs)
+
+    def transform(self,
+                  geometry_vectors: numpy.ndarray,
+                  padding_type: str = 'replication',
+                  with_mean: bool = True,
+                  with_std: bool = True
+                  ) -> numpy.ndarray:
+
+        assert type(geometry_vectors) == numpy.ndarray, 'Please provide a numpy ndarray'
+        assert numpy.ndim(geometry_vectors) == 3, 'Got a vector of rank {}. Please provide a 3d numpy ndarray ' \
+                                                  'with axes 0:batch, 1:geometries, 2:points.'\
+                                                  .format(numpy.ndim(geometry_vectors))
+        assert self.scale_factor, 'Please run the fit() method first before calling this transform method.'
+        assert padding_type in PADDING_TYPES, 'Please supply a padding type in {}'.format(PADDING_TYPES)
+
+        localized = numpy.copy(geometry_vectors)
+        means = numpy.array([localized_mean(v) for v in geometry_vectors])
+
+        for index, geometry in enumerate(localized):
+            if padding_type == 'replication':
+                if with_mean:
+                    geometry[..., :2] -= means[index]
+
+                if with_std:
+                    geometry[..., :2] /= self.scale_factor
+
+            else:  # The only remaining option being zero-padding:
+                stop_index = get_full_stop_index(geometry)
+                if with_mean:
+                    geometry[:stop_index, :2] -= means[index]
+
+                if with_std:
+                    geometry[:stop_index, :2] /= self.scale_factor
+
+        return localized
--- deep_geometry/test_geom_scaler.py
+++ deep_geometry/test_geom_scaler.py
@@ -1,13 +1,15 @@
 import unittest
 
-import numpy as np
+import numpy
 
-import deep_geometry.geom_scaler as gs
+from deep_geometry import GeomScaler
 
 # noinspection PyUnresolvedReferences
-dummy_geom = np.zeros((1, 1, 5))
+from deep_geometry.geom_scaler import localized_mean
 
-square = np.array([[
+dummy_geom = numpy.zeros((1, 1, 5))
+
+square = numpy.array([[
     [0., 0., 1., 0., 0.],
     [1., 0., 1., 0., 0.],
     [1., 1., 1., 0., 0.],
@@ -15,7 +17,7 @@ square = np.array([[
     [0., 0., 0., 0., 1.],
 ]])
 
-square_duplicate_nodes = np.array([[
+square_duplicate_nodes = numpy.array([[
     [0., 0., 1., 0., 0.],
     [1., 0., 1., 0., 0.],
     [1., 1., 1., 0., 0.],
@@ -27,7 +29,7 @@ square_duplicate_nodes = np.array([[
     [0., 0., 0., 0., 1.],
 ]])
 
-rectangle = np.array([[
+rectangle = numpy.array([[
     [0., 0., 1., 0., 0.],
     [1., 0., 1., 0., 0.],
     [1., 2., 1., 0., 0.],
@@ -35,57 +37,105 @@ rectangle = np.array([[
     [0., 0., 0., 0., 1.],
 ]])
 
-normalized_square = np.array([[
+normalized_square = numpy.array([[
     [-1., -1., 1., 0., 0.],
-    [ 1., -1., 1., 0., 0.],
-    [ 1.,  1., 1., 0., 0.],
-    [-1.,  1., 1., 0., 0.],
+    [1., -1., 1., 0., 0.],
+    [1., 1., 1., 0., 0.],
+    [-1., 1., 1., 0., 0.],
     [-1., -1., 0., 0., 1.],
 ]])
 
 
 class TestGeomScaler(unittest.TestCase):
-    def test_localized_mean(self):
-        means = gs.localized_mean(square)
-        for mean in means[0]:
-            self.assertTrue((mean == 0.5).all())
+    def test_localized_mean(self) -> None:
+        with self.subTest('It rejects inputs other than numpy ndarrays'):
+            with self.assertRaises(AssertionError):
+                localized_mean(numpy.array([1]))
 
-    def test_localized_mean_rectangle(self):
-        means = gs.localized_mean(rectangle)
-        self.assertEqual(means[0, 0, 0], 0.5)
-        self.assertEqual(means[0, 0, 1], 1)
+        with self.subTest('It rejects geometry vectors if it has too many dimensions'):
+            with self.assertRaises(AssertionError):
+                localized_mean(numpy.zeros((1, 2, 3)))
 
-    def test_localized_mean_dup_nodes(self):
-        means = gs.localized_mean(square_duplicate_nodes)
-        self.assertTrue((means == 0.75).all())
+        with self.subTest('It produces a 1d numpy array of shape (2,)'):
+            means = localized_mean(square[0])
+            self.assertEqual(numpy.ndim(means), 1)
 
-    def test_scaling_square(self):
-        scale = gs.fit(square)
-        self.assertEqual(scale, 0.5)
+        with self.subTest('It computes the centroid of a sample square'):
+            means = localized_mean(square[0])
+            numpy.testing.assert_array_equal(means, 0.5)
 
-    def test_scaling_square_dup_nodes(self):
-        scale = gs.fit(square_duplicate_nodes)
-        self.assertEqual(scale, 0.5)
+    def test_localized_mean_rectangle(self) -> None:
+        means = localized_mean(rectangle[0])
+        self.assertEqual(means[0], 0.5)
+        self.assertEqual(means[1], 1)
 
-    def test_transform(self):
-        # scaled_square = square[0] * 2
-        # scaled_square[4, 12] = 1.
-        scale = gs.fit(square)
-        n_square = gs.transform(square, scale=scale)
-        self.assertTrue((n_square == normalized_square).all())
-        coords = [geom[:, :2].flatten() for geom in n_square]
-        coords = [item for sublist in coords for item in sublist]
-        std = np.std(coords)
-        self.assertAlmostEqual(std, 1., 1)
+    def test_localized_mean_dup_nodes(self) -> None:
+        means = localized_mean(square_duplicate_nodes[0])
+        numpy.testing.assert_array_equal(means, 0.75)
 
-    def test_upsized_transform(self):
+    def test_scaling_square(self) -> None:
+        gs = GeomScaler()
+        gs.fit(square)
+        self.assertEqual(gs.scale_factor, 0.5)
+
+    def test_scaling_square_dup_nodes(self) -> None:
+        gs = GeomScaler()
+        gs.fit(square_duplicate_nodes)
+        self.assertEqual(gs.scale_factor, 0.5)
+
+    def test_transform(self) -> None:
+        with self.subTest('It rejects unfitted geometry scalers on transform'):
+            with self.assertRaises(AssertionError):
+                gs = GeomScaler()
+                gs.transform(square)
+
+        with self.subTest('It transforms a sample square'):
+            gs = GeomScaler()
+            # scaled_square = square[0] * 2
+            # scaled_square[4, 12] = 1.
+            gs.fit(square)
+            n_square = gs.transform(square)
+            numpy.testing.assert_array_equal(n_square, normalized_square)
+
+            coords = [geom[:, :2].flatten() for geom in n_square]
+            coords = [item for sublist in coords for item in sublist]
+            std = numpy.std(coords)
+            numpy.testing.assert_array_almost_equal(std, 1., 1)
+
+    def test_upsized_transform(self) -> None:
+        gs = GeomScaler()
         square_0 = square[0] * 2
         square_0[:4, 2] = 1.
         square_0[4, 4] = 1.
-        scale = gs.fit([square_0])
-        n_square = gs.transform([square_0], scale=scale)
-        self.assertTrue((n_square == normalized_square).all())
+        gs.fit(numpy.array([square_0]))
+        n_square = gs.transform(numpy.array([square_0]))
+        numpy.testing.assert_array_equal(n_square, normalized_square)
         coords = [geom[:, :2].flatten() for geom in n_square]
         coords = [item for sublist in coords for item in sublist]
-        std = np.std(coords)
-        self.assertAlmostEqual(std, 1., 1)
+        std = numpy.std(coords)
+        numpy.testing.assert_array_almost_equal(std, 1., 1)
+
+    def test_zero_padded_transform(self) -> None:
+        gs = GeomScaler()
+        gs.fit(square)
+        zero_padding = numpy.repeat(square[:1, -1], repeats=4, axis=0)
+        padded_square = numpy.concatenate([square[0], zero_padding], axis=0)
+        normalized_square = gs.transform(numpy.array([padded_square]), padding_type='zero')
+        numpy.testing.assert_array_equal(normalized_square[0, -4:], zero_padding)
+
+    def test_no_centering(self) -> None:
+        gs = GeomScaler()
+        gs.fit(square)
+        test_square = numpy.copy(square)
+        test_square[..., :2] += 2
+        gs.scale_factor = 1.  # manually override scale for testing
+        normalized_without_centering_square = gs.transform(test_square, with_mean=False)
+        numpy.testing.assert_array_equal(test_square, normalized_without_centering_square)
+
+    def test_no_scaling(self) -> None:
+        gs = GeomScaler()
+        gs.fit(square)
+        test_square = numpy.copy(square)
+        test_square[..., :2] -= 0.5
+        normalized_without_scaling_square = gs.transform(test_square, with_std=False)
+        numpy.testing.assert_array_equal(test_square, normalized_without_scaling_square)
